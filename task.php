958b4dba994f7ca3acacde073b9e2ed839138cdf
kameie <87402102+kameie@users.noreply.github.com>
2021-07-20 11:25:16 +0900

    課題のQ1からQ5の回答です

--- task.php
+++ task.php
@@ -2,17 +2,39 @@
 # 回答の出力を確認される際は，「php task.php」をターミナルから実行して下さい。
 
 print("#####q1#####".PHP_EOL);
+
 $names = ["田中", "佐藤", "佐々木", "高橋"];
 
   # 以下に回答を記載
 
+<?
+
+$names = ["田中", "佐藤", "佐々木", "高橋"];
+
+array_push($names,"斎藤");
+print_r($names);
+
+?>
+
 echo PHP_EOL;
 
 print("#####q2#####".PHP_EOL);
+
+
 $array1 = ["dog", "cat", "fish"];
 $array2 = ["bird", "bat", "tiger"];
 
   # 以下に回答を記載
+<?
+
+$array1 = ["dog", "cat", "fish"];
+$array2 = ["bird", "bat", "tiger"];
+
+$array = array_merge ($array1, $array2);
+
+print_r($array);
+
+?>
 
 echo PHP_EOL;
 
@@ -21,12 +43,39 @@ $numbers = [1, 5, 8, 10, 2, 3, 2, 3, 1, 4, 5, 9];
 
   # 以下に回答を記載
 
+<?
+
+$numbers = [1, 5, 8, 10, 2, 3, 2, 3, 1, 4, 5, 9];
+
+$count = 0;
+foreach($numbers as $number){
+ if($number === 3){
+    $count++;
+ }
+}
+print_r($count . "回".PHP_EOL);
+?>
+
+
 echo PHP_EOL;
 
 print("#####q4#####".PHP_EOL);
+
 $sports = ["サッカー", "フットサル", null, "野球", "バスケ", null, "バレー"];
 
   # 以下に回答を記載
+
+<?
+
+$sports = ["サッカー", "フットサル", null, "野球", "バスケ", null, "バレー"];
+
+foreach($sports as $key => $value){
+     if($value == 'null'){
+         unset($sports[$key]);
+     }
+}
+
+print_r($sports);
 
 echo PHP_EOL;
 
@@ -35,6 +84,16 @@ $array1 = [];
 $array2 = [1, 5, 8, 10];
 
   # 以下に回答を記載
+
+$array1 = [];
+var_export(empty($array1));
+
+echo PHP_EOL;
+
+$array2 = [1, 5, 8, 10];
+var_export(empty($array2));
+
+echo PHP_EOL;
 
 echo PHP_EOL;
 
@@ -140,6 +199,8 @@ class User
 
 $user1 = new User("神里", 32, "男");
 $user2 = new User("あじー", 32, "男");
+
+
 
 $user1->info();
 print("-------------".PHP_EOL);
